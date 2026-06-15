8ab83a071da83ae171dec4a00cce81690ab1dd7a
Tanmay Paliwal <tanmayprofessional576@gmail.com>
2026-06-16 01:57:59 +0530

    Learned Dynamic Routes

--- project-1/src/app/blog/2/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const blog2 = () => {
-  return (
-    <div>blog2</div>
-  )
-}
-
-export default blog2
--- project-1/src/app/blog/[id]/page.tsx
+++ project-1/src/app/blog/[id]/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const page = () => {
+const page = async ({params} : {params: Promise <{id : string}> }) => {
+    const {id} = await params
   return (
-    <div>Dynamic Page</div>
+    <div>Dynamic Page {id}</div>
   )
 }
 
